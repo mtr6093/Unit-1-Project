0c36780ac38079c1699388cba54bfa1981fc5ca1
Rivers Marc <m.t.rivers@hotmail.com>
2021-01-20 10:32:53 -0600

    stopping to look at flipping cards

--- script.js
+++ script.js
@@ -18,14 +18,20 @@ cards.push(new card('2', 'How many crew memebers fly in the B-1b?', '4'))
 console.log(cards[0].A)
 
 let qDisplay = document.getElementById("question")
-
+let aDisplay = document.getElementById("answer")
 // console.log(cDisplay)
 
+aDisplay.textContent = cards[0].A
 qDisplay.textContent = cards[0].Q
 
 let fButton = document.getElementById('flip')
 
-fButton.addEventListener('click', flip)
+
+fButton.addEventListener('click', flip) 
+
+function flip() {
+    document.getElementById('question').style.display.none
+}
 
 let cButton = document.getElementById('cor')
 let iButton = document.getElementById('inc')
