c33000b491c19332aa497630ea3f60ccb498326a
Rivers Marc <m.t.rivers@hotmail.com>
2021-01-20 10:20:20 -0600

    started CSS and flip functionality

--- script.js
+++ script.js
@@ -17,11 +17,11 @@ cards.push(new card('2', 'How many crew memebers fly in the B-1b?', '4'))
 
 console.log(cards[0].A)
 
-let cDisplay = document.getElementById("d1")
+let qDisplay = document.getElementById("question")
 
-console.log(cDisplay)
+// console.log(cDisplay)
 
-cDisplay.textContent = cards[0].Q
+qDisplay.textContent = cards[0].Q
 
 let fButton = document.getElementById('flip')
 
@@ -30,8 +30,9 @@ fButton.addEventListener('click', flip)
 let cButton = document.getElementById('cor')
 let iButton = document.getElementById('inc')
 // function to flip card
-let cScore= 1
+let cScore = 1
 let cScoreDisplay = document.getElementById('crc').textContent += cScore
+console.log("C", cScore)
 
 let iScore = 3
 let iScoreDisplay = document.getElementById('icrc').textContent += iScore
@@ -39,18 +40,20 @@ let iScoreDisplay = document.getElementById('icrc').textContent += iScore
 cButton.addEventListener('click', correct )
 function correct(){
     console.log("c clicked")
+   
     if (iScore == 0){
-        cScore ++
+        cScore = cScore +1
         console.log(cScore)
         console.log(iScore)
-        return(cScore)
-        
+        return(cScore)       
     }else {
         cScore = cScore +1
         iScore = iScore -1
         console.log(cScore)
         console.log(iScore)
+        return(cScore)
     }
+    
 }
 
 
