b06c662013d87016a8a1cee2323516ade4d521ae
Rivers Marc <m.t.rivers@hotmail.com>
2021-01-21 21:14:41 -0600

    CSS Work

--- script2.js
+++ script2.js
@@ -1,9 +1,6 @@
 
 let accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
 
-
-
-
 accordionItemHeaders.forEach(accordionItemHeader => {
     accordionItemHeader.addEventListener("click", event => {
         accordionItemHeader.classList.toggle("active");
